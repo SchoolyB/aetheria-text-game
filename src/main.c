82c324423a9ab68db875353db452643ba00121b4
SchoolyB <marshallbcodes@gmail.com>
2023-09-26 19:08:44 -0500

    Update main function

--- src/main.c
+++ src/main.c
@@ -5,20 +5,14 @@
 #include "./Source/Chapter1.c"
 #include "./Utils/Functions/Escape.c"
 #include "./Utils/Functions/Logger.c"
-#include "./Utils/Functions/Fight.c"
 #include "./Utils/RootCommandLine.c"
 #include "./Utils/InGameCommandLine.c"
-// Function to log messages to a file
 
 int main()
 {
-    FILE *logFile = fopen("logs/runtime.log", "a"); // "a" mode appends to the file if it exists
-    if (logFile == NULL)
-    {
-        perror("Error opening the log file");
-        exit(1);
-    }
+    CREATE_LOG_FILE(logFile, "logs/runtime.log");    
     ROOT_LEVEL_COMMAND_LINE(logFile); //starting point
+    // IN_GAME_COMMAND_LINE(logFile);
     logMessage(logFile, "Program ended.");
     fclose(logFile);
     return 0;
