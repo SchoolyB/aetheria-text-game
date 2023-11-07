61bda5a0cbf2062d68177a56c47c9cbb02e3e64e
SchoolyB <marshallbcodes@gmail.com>
2023-11-07 10:11:50 -0600

    Install Vite, Tailwind, & DaisyUI

--- client/src/script.js
+++ client/src/script.js
@@ -1,14 +1,27 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const footerParagraph = document.getElementById('build_version');
-
-    const filePath = './VERSION';
-
-    fetch(filePath)
-        .then(response => response.text())
+// Function to fetch and display version information
+function displayVersionInfo() {
+    fetch('../../version.json') // Assuming version.json is in the same directory
+        .then(response => response.json())
         .then(data => {
-            footerParagraph.textContent = data;
+            // Access the version data from the JSON
+            const projectVersion = data.game_version;
+            const websiteVersion = data.website_version;
+
+            // Create HTML elements to display the data
+            const versionInfo = document.getElementById('version-info');
+            const projectVersionElement = document.createElement('p');
+            projectVersionElement.textContent = `Current Game Build: ${projectVersion}`;
+            const websiteVersionElement = document.createElement('p');
+            websiteVersionElement.textContent = `Current Website Build: ${websiteVersion}`;
+
+            // Append the elements to the page
+            versionInfo.appendChild(projectVersionElement);
+            versionInfo.appendChild(websiteVersionElement);
         })
         .catch(error => {
-            console.error('Error:', error);
+            console.error('Error fetching version data:', error);
         });
-});
+}
+
+// Call the function to display version information
+displayVersionInfo();
