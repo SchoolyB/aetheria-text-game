67b28f7c9699077032bfa2aaca11f9dfe0250e41
SchoolyB <marshallbcodes@gmail.com>
2023-11-07 14:23:24 -0600

    Minor changes

--- client/src/script.js
+++ client/src/script.js
@@ -5,5 +5,6 @@ const gameVersionElement = document.getElementById('game-version');
 const websiteVersionElement = document.getElementById('website-version');
 
 // Update the content of the HTML elements with the fetched data
-gameVersionElement.textContent = `Current Game Build: ${versionData.game_version}`;
-websiteVersionElement.textContent = `Current Website Build: ${versionData.website_version}`;
+gameVersionElement.textContent = `${versionData.game_version}`;
+websiteVersionElement.textContent = `${versionData.website_version}`;
+
