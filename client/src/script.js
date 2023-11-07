6ee88c97c0e16b8c594fef360e8da666f5921eb1
SchoolyB <marshallbcodes@gmail.com>
2023-11-07 11:07:24 -0600

    More work on website

--- client/src/script.js
+++ client/src/script.js
@@ -1,27 +1,9 @@
-// Function to fetch and display version information
-function displayVersionInfo() {
-    fetch('../../version.json') // Assuming version.json is in the same directory
-        .then(response => response.json())
-        .then(data => {
-            // Access the version data from the JSON
-            const projectVersion = data.game_version;
-            const websiteVersion = data.website_version;
+import versionData from '../../version.json';
 
-            // Create HTML elements to display the data
-            const versionInfo = document.getElementById('version-info');
-            const projectVersionElement = document.createElement('p');
-            projectVersionElement.textContent = `Current Game Build: ${projectVersion}`;
-            const websiteVersionElement = document.createElement('p');
-            websiteVersionElement.textContent = `Current Website Build: ${websiteVersion}`;
+// Get references to the HTML elements by their IDs
+const gameVersionElement = document.getElementById('game-version');
+const websiteVersionElement = document.getElementById('website-version');
 
-            // Append the elements to the page
-            versionInfo.appendChild(projectVersionElement);
-            versionInfo.appendChild(websiteVersionElement);
-        })
-        .catch(error => {
-            console.error('Error fetching version data:', error);
-        });
-}
-
-// Call the function to display version information
-displayVersionInfo();
+// Update the content of the HTML elements with the fetched data
+gameVersionElement.textContent = `Current Game Build: ${versionData.game_version}`;
+websiteVersionElement.textContent = `Current Website Build: ${versionData.website_version}`;
