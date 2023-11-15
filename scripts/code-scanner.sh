0dd9bc05cd4520a17295221b93fb3480e565822d
Marshall <110133622+SchoolyB@users.noreply.github.com>
2023-11-15 09:45:09 -0600

    Update code-scanner.sh

--- scripts/code-scanner.sh
+++ scripts/code-scanner.sh
@@ -5,12 +5,6 @@ gcc -o Aetheria.exe src/main.c -lncurses
 
 if [ $? -eq 0 ]; then
   echo -e "\e[32mBuild Successful!\e[0m"
-  sleep 1
-  echo "Clearing Terminal"
-  sleep 1
-  clear
-  echo "Running Aetheria..."
-  ./Aetheria.exe
 else
   echo -e "\e[31mBuild Failed.\e[0m"
   exit 1
