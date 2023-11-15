1bca420d8f3f4eeece04441a04a2eeab5c8fd75b
Marshall <110133622+SchoolyB@users.noreply.github.com>
2023-11-15 09:42:08 -0600

    Update code-scanner.sh

--- scripts/code-scanner.sh
+++ scripts/code-scanner.sh
@@ -1,15 +1,17 @@
-# !This is only used for code scanning actions on GitHub
-gcc -o ../game/Aetheria.exe ../game/src/main.c -lncurses
+cd ../game
+
+# Build the Aetheria executable
+gcc -o Aetheria.exe src/main.c -lncurses
 
 if [ $? -eq 0 ]; then
-    echo "\e[32mBuild Successful!\e[0m"
-    sleep 1
-    echo "Clearing Terminal"
-    sleep 1
-    clear
-    echo "Running Aetheria..."
-    ../game/Aetheria.exe
+  echo -e "\e[32mBuild Successful!\e[0m"
+  sleep 1
+  echo "Clearing Terminal"
+  sleep 1
+  clear
+  echo "Running Aetheria..."
+  ./Aetheria.exe
 else
-    echo "\e[31mBuild Failed.\e[0m"
-    exit 1
+  echo -e "\e[31mBuild Failed.\e[0m"
+  exit 1
 fi
